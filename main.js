bfedb574df053da04cdb7edb1500f55b8ca347ac
AerospacedOut <michael.j.winebrenner@gmail.com>
2021-03-04 14:41:30 +0000

    "Minor changes"

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 'use strict'
 
 import readline from 'readline';
-const readLine = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function askQuestion(query) {
+    const readLine = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    return new Promise(resolve => readLine.question(query, ans => {
+        readLine.close();
+        resolve(ans);
+    }))
+}
+
+var userDone = false;
+
+// function awaitUser() {
+//     while (userDone == false) {
+
+//     }
+//     userDone = false;
+// }
 
 let player = {
-        health: 3,
-        inventory: [],
-        pName: ""
-    }
-    //Intro (runs once)
-readLine.question('Who are you?', myName => {
-    player.pName = myName;
-    readLine.close();
-});
-console.log('Hello ' + player.pName)
-
+    health: 3,
+    inventory: [],
+    pName: ""
+}
+//Intro (runs once)
+player.pName = askQuestion('Who are you?\n');
+console.log('Hello ')
 
 //Room Loop (can break out of and go to other room)
 
