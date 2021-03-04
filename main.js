1d68952e298208755293cb091fb1fe299c07c83f
AerospacedOut <michael.j.winebrenner@gmail.com>
2021-03-04 16:04:11 +0000

    basic rooms setup

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ function askQuestion(query) {
     }))
 }
 
-var userDone = false;
-
 // function awaitUser() {
 //     while (userDone == false) {
 
@@ -24,14 +22,52 @@ var userDone = false;
 let player = {
     health: 3,
     inventory: [],
-    pName: ""
+    pName: "",
+    currentRoom: "south" // valid: south, east, west, north, central
 }
 //Intro (runs once)
-player.pName = askQuestion('Who are you?\n');
-console.log('Hello ')
+player.pName = await askQuestion('Who are you?\n');
+console.log('Hello ' + player.pName)
 
-//Room Loop (can break out of and go to other room)
+rooms:
+while (player.health > 0) {
+    //Room Loop (can break out of and go to other room)
+    while (player.currentRoom = 'south') {
+        // when player leaves, break and goto chosen room
+        let req = await askQuestion('What would you like to do?\n'); // list options
+        // if req contains leave room or central room then goto central room (regex?)
 
-//Room loop 2, etc.
+        // Puzzle here
+
+        console.log('In room S')
+        player.currentRoom = 'central';
+        break;
+    };
+
+    //Room loop 2, etc.
+    while (player.currentRoom = 'central') {
+        // when player leaves, break and goto chosen room
+        console.log('In room C')
+        break
+    };
+} // End of rooms
 
 //Closing loop (at least one)
+
+// ------- Room definitions here ------
+class Room {
+    constructor() {
+        this.removable = [];
+        this.puzzles = {};
+    }
+    remove(item) {
+        removed = this.removable[item];
+        delete this.removable[item];
+        return removed
+    }
+}
+
+function pickUp(item) {
+    player.inventory.push(item);
+    console.log('Picked up ' + item);
+}
