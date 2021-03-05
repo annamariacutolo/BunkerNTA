6a37db5505b09cdbb44c028c894c5a32cf2b0aba
AerospacedOut <michael.j.winebrenner@gmail.com>
2021-03-05 10:39:55 +0000

    added central room as a module

---
+++ centralmod.js
@@ -0,0 +1,13 @@
+const flavourText = `You enter the central atrium. The soft light from the emergency lights provides barely adequate visibility.
+There's a stairwell in the centre looks like you could use it to get to the surface above.
+You see that there are four doors leading to rooms labelled north to south that you could go to.
+Your living quarters appear to have been in the south room.
+You may take any of the following actions:
+    1) Go to the west room
+    2) Go to the north room
+    3) Go to the east room
+    4) Go to the south room
+    5) Go up the stairwell
+    6) Check the room`
+
+let roomCentral = new Room("Central", flavourText)
